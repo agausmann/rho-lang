bc15e4e0ca03f03011b53ad488e8f2c33ca6953d
Adam Gausmann <agausmann@fastmail.com>
2021-08-26 22:42:01 -0500

    Start implementing expression parsing

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,16 @@ const DIGIT: &str = "0123456789";
 const HEXDIGIT: &str = "0123456789ABCDEFabcdef";
 const BINDIGIT: &str = "01";
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    String(String),
+    Int(i64),
+    Float(f64),
+}
+
+type Error<T> = Simple<T>;
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Whitespace,
     LeftParen,
@@ -38,33 +47,33 @@ pub enum Token {
     String(String),
 }
 
-fn nonzero_digit() -> impl Parser<char, char, Error = Simple<char>> {
+fn nonzero_digit() -> impl Parser<char, char, Error = Error<char>> {
     one_of(NONZERO_DIGIT.chars())
 }
 
-fn digit() -> impl Parser<char, char, Error = Simple<char>> {
+fn digit() -> impl Parser<char, char, Error = Error<char>> {
     one_of(DIGIT.chars())
 }
 
-fn hexdigit() -> impl Parser<char, char, Error = Simple<char>> {
+fn hexdigit() -> impl Parser<char, char, Error = Error<char>> {
     one_of(HEXDIGIT.chars())
 }
 
-fn bindigit() -> impl Parser<char, char, Error = Simple<char>> {
+fn bindigit() -> impl Parser<char, char, Error = Error<char>> {
     one_of(BINDIGIT.chars())
 }
 
-fn float_exponent() -> impl Parser<char, Vec<char>, Error = Simple<char>> {
+fn float_exponent() -> impl Parser<char, Vec<char>, Error = Error<char>> {
     one_of("eE".chars())
         .chain(one_of("+-".chars()).or_not())
         .chain(digit().repeated_at_least(1))
 }
 
-fn float_after_decimal() -> impl Parser<char, Vec<char>, Error = Simple<char>> {
+fn float_after_decimal() -> impl Parser<char, Vec<char>, Error = Error<char>> {
     digit().repeated_at_least(1)
 }
 
-fn float_int_suffix() -> impl Parser<char, Vec<char>, Error = Simple<char>> {
+fn float_int_suffix() -> impl Parser<char, Vec<char>, Error = Error<char>> {
     (just('.')
         .chain(float_after_decimal().or_not().flatten())
         .or_not()
@@ -81,7 +90,7 @@ fn parse_number(string: String) -> Token {
 }
 
 //TODO: Floats starting with a decimal point (.)
-fn leading_digit_number() -> impl Parser<char, Token, Error = Simple<char>> {
+fn leading_digit_number() -> impl Parser<char, Token, Error = Error<char>> {
     (nonzero_digit()
         .chain(digit().repeated())
         .chain::<char, _, _>(float_int_suffix())
@@ -108,7 +117,7 @@ fn leading_digit_number() -> impl Parser<char, Token, Error = Simple<char>> {
     ))
 }
 
-fn string_contents() -> impl Parser<char, Token, Error = Simple<char>> {
+fn string_contents() -> impl Parser<char, Token, Error = Error<char>> {
     (none_of("\"\\".chars()).map(Some))
         .or(just('\\').padding_for(
             // Escape codes
@@ -126,7 +135,7 @@ fn string_contents() -> impl Parser<char, Token, Error = Simple<char>> {
                     }))
                 .map(Some)
                 // Ignore all whitespace (including newlines) after a backslash
-                .or(one_of(WHITESPACE.chars()).repeated_at_least(1).to(None))
+                .or(one_of(WHITESPACE.chars()).repeated_at_least(1).to(None)),
         ))
         .repeated()
         .flatten()
@@ -134,7 +143,7 @@ fn string_contents() -> impl Parser<char, Token, Error = Simple<char>> {
         .map(Token::String)
 }
 
-fn tokenizer() -> impl Parser<char, Token, Error = Simple<char>> {
+fn tokenizer() -> impl Parser<char, Token, Error = Error<char>> {
     (one_of(WHITESPACE.chars())
         .repeated_at_least(1)
         .to(Token::Whitespace))
@@ -201,40 +210,184 @@ fn tokenizer() -> impl Parser<char, Token, Error = Simple<char>> {
         .padded_by(just('"')))
 }
 
-pub fn get_tokens(s: &str) -> Result<Vec<Token>, Vec<Simple<char>>> {
+pub fn get_tokens(s: &str) -> Result<Vec<Token>, Vec<Error<char>>> {
     tokenizer().repeated().padded_by(end()).parse(s)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    And,
+    Or,
+    BitAnd,
+    BitOr,
+    BitXor,
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+}
+
+fn binary_op() -> impl Parser<Token, BinaryOp, Error = Error<Token>> {
+    (just(Token::Plus).to(BinaryOp::Add))
+        .or(just(Token::Minus).to(BinaryOp::Sub))
+        .or(just(Token::Asterisk).to(BinaryOp::Mul))
+        .or(just(Token::Slash).to(BinaryOp::Div))
+        .or(just(Token::Percent).to(BinaryOp::Mod))
+        .or(just(Token::And)
+            .then(just(Token::And).to(BinaryOp::And).or_not())
+            .map(|(_, op)| op.unwrap_or(BinaryOp::BitAnd)))
+        .or(just(Token::Pipe)
+            .then(just(Token::Pipe).to(BinaryOp::Or).or_not())
+            .map(|(_, op)| op.unwrap_or(BinaryOp::BitOr)))
+        .or(just(Token::Caret).to(BinaryOp::BitXor))
+        .or(just(Token::Equal).then(just(Token::Equal)).to(BinaryOp::Eq))
+        .or(just(Token::Exclamation)
+            .then(just(Token::Equal))
+            .to(BinaryOp::Ne))
+        .or(just(Token::Less)
+            .then(just(Token::Equal).to(BinaryOp::Le).or_not())
+            .map(|(_, op)| op.unwrap_or(BinaryOp::Lt)))
+        .or(just(Token::Greater)
+            .then(just(Token::Greater).to(BinaryOp::Ge).or_not())
+            .map(|(_, op)| op.unwrap_or(BinaryOp::Gt)))
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnaryOp {
+    Pos,
+    Neg,
+    Not,
+}
+
+fn unary_op() -> impl Parser<Token, UnaryOp, Error = Error<Token>> {
+    (just(Token::Plus).to(UnaryOp::Pos))
+        .or(just(Token::Minus).to(UnaryOp::Neg))
+        .or(just(Token::Exclamation).to(UnaryOp::Not))
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Invalid,
+    Literal(Value),
+    Variable(String),
+    BinaryOps(Box<Expr>, Vec<(BinaryOp, Expr)>),
+    UnaryOps(Box<Expr>, Vec<UnaryOp>),
+}
+
+pub fn expr() -> impl Parser<Token, Expr, Error = Error<Token>> {
+    recursive(|expr| {
+        // Sub-parser that parses anything except binary operators.
+        // This is used to prevent the sub-expressions of a binary operator from parsing
+        // operators that we want the parent to parse.
+        let term = recursive(|term| {
+            (just::<_, Simple<Token>>(Token::Whitespace).repeated()).padding_for(
+                filter_map(|span, token| match token {
+                    Token::Identifier(name) => Ok(Expr::Variable(name)),
+                    Token::String(value) => Ok(Expr::Literal(Value::String(value))),
+                    Token::Int(value) => Ok(Expr::Literal(Value::Int(value))),
+                    Token::Float(value) => Ok(Expr::Literal(Value::Float(value))),
+                    _ => Err(Error::expected_token_found(Some(span), vec![], Some(token))),
+                })
+                // Allow parsing a full expression if it is enclosed in parentheses.
+                .or((expr.clone())
+                    .delimited_by(Token::LeftParen, Token::RightParen)
+                    .map(|option| option.unwrap_or(Expr::Invalid)))
+                .or(unary_op()
+                    .repeated_at_least(1)
+                    .then(term)
+                    .map(|(ops, base)| Expr::UnaryOps(Box::new(base), ops)))
+                // Handle trailing whitespace
+                .padded_by(just(Token::Whitespace).repeated()),
+            )
+        });
+
+        (term.clone())
+            // Handle trailing binary operators
+            .then(
+                binary_op()
+                    .then(term.clone())
+                    .repeated_at_least(1)
+                    .or_not(),
+            )
+            .map(|(base, maybe_ops)| match maybe_ops {
+                Some(ops) => Expr::BinaryOps(Box::new(base), ops),
+                None => base,
+            })
+    })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn assert_parse(input: &str) {
+    fn assert_tokens(input: &str) {
         dbg!(get_tokens(input).unwrap());
     }
 
     #[test]
     fn comment() {
-        assert_parse("// This is a line_comment");
+        assert_tokens("// This is a line_comment");
     }
 
     #[test]
     fn block_comment() {
-        assert_parse("/* This is a block comment */");
+        assert_tokens("/* This is a block comment */");
     }
 
     #[test]
     fn block_comment_with_asterisk() {
-        assert_parse("/** This *is* a block comment with embedded asterisks. **/");
+        assert_tokens("/** This *is* a block comment with embedded asterisks. **/");
     }
 
     #[test]
     fn string() {
-        assert_parse(r#" "Hello World" "#);
+        assert_tokens(r#" "Hello World" "#);
     }
 
     #[test]
     fn string_with_escapes() {
-        assert_parse(r#" "Hello\r\n\    \t \"World!\"\x0A" "#);
+        assert_tokens(r#" "Hello\r\n\    \t \"World!\"\x0A" "#);
+    }
+
+    fn assert_expr(input: &str, expected: Expr) {
+        let tokens = get_tokens(input).unwrap();
+        let parsed = expr().padded_by(end()).parse(tokens).unwrap();
+        assert_eq!(expected, parsed);
+    }
+
+    #[test]
+    fn two_plus_two() {
+        assert_expr(
+            "2 + 2",
+            Expr::BinaryOps(
+                Box::new(Expr::Literal(Value::Int(2))),
+                vec![(BinaryOp::Add, Expr::Literal(Value::Int(2)))],
+            ),
+        );
+    }
+
+    #[test]
+    fn multiple_operators() {
+        assert_expr(
+            "1 - 2 + -3 - -!4 * 5 / 6 % 7",
+            Expr::BinaryOps(
+                Box::new(Expr::Literal(Value::Int(1))),
+                vec![
+                    (BinaryOp::Sub, Expr::Literal(Value::Int(2))),
+                    (BinaryOp::Add, Expr::UnaryOps(Box::new(Expr::Literal(Value::Int(3))), vec![UnaryOp::Neg])),
+                    (BinaryOp::Sub, Expr::UnaryOps(Box::new(Expr::Literal(Value::Int(4))), vec![UnaryOp::Neg, UnaryOp::Not])),
+                    (BinaryOp::Mul, Expr::Literal(Value::Int(5))),
+                    (BinaryOp::Div, Expr::Literal(Value::Int(6))),
+                    (BinaryOp::Mod, Expr::Literal(Value::Int(7))),
+                ],
+            ),
+        );
     }
 }
